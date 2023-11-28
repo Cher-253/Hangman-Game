02c673ac5126bd595a2ac32a8383a83cbdeb96fb
Cher-253 <56059898+Cher-253@users.noreply.github.com>
2023-11-28 10:18:59 +0530

    Update GameLib.h

--- GameLib.h
+++ GameLib.h
@@ -1,11 +1,24 @@
 #ifndef _GAMELIB_H
 #define _GAMELIB_H
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "GameLib.h"
 #define MAX_INPUT 81
 #define STRIKES 3
-void StartGame(char ChosenPhrase[]);
+#define MAX_ENTRIES 10 
+
+typedef struct  
+{
+    char *Artist;
+    char *SongTitle;
+    char *AlbumName;
+    char ReleaseDate[5];
+}GuessSong;
+
+int StartGame(char ChosenPhrase[MAX_INPUT], FILE *filehandleptr,GuessSong StructArray[MAX_ENTRIES]);
 int GuessALetter(char Phrase[],char PhraseCopy[],char UpperPhrase[]);
 void DashIt(char *Phrase, char DashPhrase[]);
 void CheckPhrase(char *Phrase);
-#endif
 
+#endif
